3846dbf270fbe73ac0d95c4e7332100dc29b6f3c
hien.nguyen <hien.nguyen@elisa.fi>
2020-04-03 20:17:45 +0300

    test tcp multithread server

--- ssh-tcp-multi-thread-server/src/main/java/test/TCPMultiThreadEchoServer.java
+++ ssh-tcp-multi-thread-server/src/main/java/test/TCPMultiThreadEchoServer.java
@@ -1,3 +1,4 @@
+package test;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,11 +13,15 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
+import static test.RuntTimeExceptionWrapper.noException;
+
 
 public class TCPMultiThreadEchoServer {
+
     private static final int TELNET_PORT = 8080;
 
     private static final String[] allowedIps = {"127.0.0.1"};
+
     final static Collection<InetAddress> allowedAddresses =
             Arrays.stream(allowedIps)
                     .map(noException(InetAddress::getByName))
@@ -24,14 +29,19 @@ public class TCPMultiThreadEchoServer {
 
     public static void main(String[] args) throws IOException {
         final ServerSocket serverSocket = new ServerSocket(TELNET_PORT);
-        final ExecutorService service = Executors.newFixedThreadPool(3);
+
+        final ExecutorService service = Executors.newFixedThreadPool(4);
+
         while (true) {
+
             final Socket clientSocket = serverSocket.accept();
             final InetSocketAddress remote = (InetSocketAddress) clientSocket.getRemoteSocketAddress();
             final InetAddress address = remote.getAddress();
             final byte[] ip = address.getAddress();
+
             System.out.println("connection from port=" + remote.getPort()
                     + " ip=" + Arrays.toString(ip));
+
             if (isAllowed(ip)) {
                 serve(service, clientSocket);
             } else {
@@ -42,6 +52,11 @@ public class TCPMultiThreadEchoServer {
         }
     }
 
+    /**
+     * Check if ip is allowed
+     * @param ip
+     * @return
+     */
     private static boolean isAllowed(byte[] ip) {
         return allowedAddresses
                 .stream()
@@ -49,12 +64,18 @@ public class TCPMultiThreadEchoServer {
                 .anyMatch(allowed -> Arrays.equals(ip, allowed));
     }
 
+
+    /**
+     *
+     * @param service
+     * @param clientSocket
+     */
     private static void serve(ExecutorService service, Socket clientSocket) {
         service.submit(
                 () -> {
                     try (final InputStream in = clientSocket.getInputStream();
                          final OutputStream out = clientSocket.getOutputStream()) {
-                        while (clientSocket.isConnected() && in.transferTo(out) > 0) ;
+//                        while (clientSocket.isConnected() && in.transferTo(out) > 0) ;
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
